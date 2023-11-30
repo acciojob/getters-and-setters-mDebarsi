3df70ab905afe6288aeef91778398ea70d8052c4
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-11-30 22:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studing`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+
+const student1 = new Student("John", 20);
+student1.study();
+
+const teacher1 = new Teacher("Professor Smile");
 
 // Do not change the code below this line
 window.Person = Person;
